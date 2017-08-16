7cef3d2ce91dbb122f43ec5f51d99b7d357687d4
YousriK <kribyous@hotmail.fr>
2017-08-16 21:49:03 +0200

    fond

---
+++ Javascript/Jsafe.js
@@ -0,0 +1,10 @@
+var header = document.querySelector("#siteWrapper header");
+
+function scrolled(){
+	var windowHeight = document.body.clientHeight,
+		currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	
+	header.className = (currentScroll >= windowHeight - header.offsetHeight) ? "fixed" : "";
+}
+
+addEventListener("scroll", scrolled, false);
